77e3d574a184e899069aa9b062f5a34df077c6d1
mazespd <131882625+mazespd@users.noreply.github.com>
2023-07-06 22:09:23 +0700

    Update sql.sh

--- sql.sh
+++ sql.sh
@@ -1,4 +1,4 @@
-mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam
+mysql --user rfamro --host mysql-rfam-public.ebi.ac.uk --port 4497 --database Rfam <<MYSQL_INPUT
 SHOW TABLES;
 exit;
 echo "END"
